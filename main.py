9e2cf444e83983123e18f470572b6b8656b11e0f
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-01-28 01:19:50 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from plistlib import FMT_BINARY
 import time
 import dotenv
 import os
@@ -7,6 +9,8 @@ import logging
 
 
 logger = logging.getLogger("logger")
+
+
 
 
 def send_message(tg_chat, bot, messages):
@@ -22,7 +26,7 @@ def send_message(tg_chat, bot, messages):
         {result}""",
             chat_id=tg_chat,
         )
-        logger.info("Отправлено сообщение")
+        logger.info("Отправлено сообщение с результатом проверки работы!")
 
 
 def get_reviews(dvmn_token, tg_chat, bot):
@@ -37,12 +41,12 @@ def get_reviews(dvmn_token, tg_chat, bot):
             )
             response.raise_for_status()
             dvmn_response = response.json()
-            logger.info(dvmn_response)
+            logger.info('Получен ответ от dvmn.org')
             if dvmn_response["status"] == "found":
                 payload = {"timestamp": dvmn_response["last_attempt_timestamp"]}
                 messages = dvmn_response["new_attempts"]
+                logger.info('Найден новый результат проверки!')
                 send_message(tg_chat, bot, messages)
-                logger.info(messages)
             elif dvmn_response["status"] == "timeout":
                 payload = {"timestamp": dvmn_response["timestamp_to_request"]}
                 logger.info(
@@ -52,20 +56,30 @@ def get_reviews(dvmn_token, tg_chat, bot):
                 )
 
         except requests.exceptions.ReadTimeout:
-            logger.exception("Перезапуск")
+            logger.exception("Перезапуск по таймауту.", exc_info=False)
+
         except requests.exceptions.ConnectionError:
             time.sleep(10)
-            logger.exception("Нет соединения, переподключение.")
+            logger.exception("Нет соединения с сетью, переподключение.", exc_info=False)
 
 
 def main():
-    logging.basicConfig(filename="bot.log", level=logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
+    logger.setLevel('INFO')
     dotenv.load_dotenv()
-    print('Бот запущен')
     dvmn_token = os.environ["DVMN_TOKEN"]
     tg_token = os.environ["TG_TOKEN"]
     tg_chat = os.environ["TG_CHAT"]
     bot = telegram.Bot(token=tg_token)
+    class MyLogsHandler(logging.Handler):
+        def emit(self, record):
+            log_entry = self.format(record)
+            bot.send_message(chat_id=tg_chat, text=log_entry)
+    tg_logger = MyLogsHandler()
+    tg_logger.setLevel(logging.INFO)
+    tg_logger.setFormatter(formatter)
+    logger.addHandler(tg_logger)
+    logger.info('Бот запущен')
     get_reviews(dvmn_token, tg_chat, bot)
 
 
